03c5cf20c095304e949f4a4675ad90d2eda8aaf6
Rocky Dunlap <rocky.dunlap@noaa.gov>
2022-07-14 02:37:21 -0600

    action=collect dir=intel_18.0.5_openmpi_g_develop branch=develop hash=v8.4.0b04

---
+++ develop/intel/18.0.5/g/openmpi/3.1.4/out/test.bat
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+#PBS -N test.bat
+#PBS -l walltime=3:00:00
+#PBS -q regular
+#PBS -A p93300606
+#PBS -l select=1:ncpus=36:mpiprocs=36
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load intel/18.0.5 openmpi/3.1.4
+module load netcdf/4.6.3
+
+set -x
+export ESMF_DIR=/glade/scratch/dunlap/esmf-test-dev/intel_18.0.5_openmpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/scratch/dunlap/esmf-test-dev/intel_18.0.5_openmpi_g_develop/module-test.log
+cd /glade/scratch/dunlap/esmf-test-dev/intel_18.0.5_openmpi_g_develop/esmf
+make install 2>&1| tee ../install.log
+make all_tests 2>&1| tee ../test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
+cd ../nuopc-app-prototypes
+./testProtos.sh 2>&1| tee ../nuopc.log
